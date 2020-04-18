f1591a0494df6653bb1e17064f9a0085df73105d
Hanz <firhan.hardiansyah09@gmail.com>
2020-04-18 08:54:38 +0700

    melengkapi bagian dari fontawesome

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
       </div>
       <!--  -->
       <div class="card-body text-dark">
-        <form action="" method="post" id="form-tiket">
+        <form action="" method="POST">
           <div class="form-row">
             <div class="form-group col-4">
               <label>Dari</label>
@@ -85,17 +85,26 @@
       $dari_mana = $_POST['dari_mana'];
       $ke_mana = $_POST['ke_mana'];
       $jml_tiket = $_POST['jml_tiket'];
+      $harga_tiket = 0;
 
       if ($jml_tiket != 0) {
         if (($dari_mana && $ke_mana) == ("jakarta" && "bandung")) {
-          $harga_tiket = 20000;
+          $harga_tiket = 21000;
           $total_harga = $harga_tiket * $jml_tiket;
             echo "Dari         : ".$dari_mana."<br>";
             echo "Ke           : ".$ke_mana."<br>";
             echo "Harga Tiket  : Rp.".$harga_tiket."<br>";
             echo "Jumlah Tiket : ".$jml_tiket."<br>";
             echo "Total Harga  : Rp.".$total_harga."<br>";
-          }
+          } else if (($dari_mana && $ke_mana) == ("bandung" && "yogyakarta")) {
+            $harga_tiket = 50000;
+            $total_harga = $harga_tiket * $jml_tiket;
+              echo "Dari         : ".$dari_mana."<br>";
+              echo "Ke           : ".$ke_mana."<br>";
+              echo "Harga Tiket  : Rp.".$harga_tiket."<br>";
+              echo "Jumlah Tiket : ".$jml_tiket."<br>";
+              echo "Total Harga  : Rp.".$total_harga."<br>";
+            }
       }  
     }
   ?>
